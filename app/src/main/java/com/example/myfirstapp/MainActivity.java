27376fb07d69ad562d6c7d420e7253f95be0e272
MadalinaStelea <madalina.i.stelea@gmail.com>
2022-09-09 16:13:10 +0300

    app

---
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mail;
+    private EditText password;
+    private Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mail = findViewById(R.id.editTextTextEmailAddress2);
+        password = findViewById(R.id.editTextTextPassword2);
+        loginButton = findViewById(R.id.button2);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Menu.class));
+            }
+        });
+    }
+}

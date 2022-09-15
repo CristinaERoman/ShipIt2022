b407e80748d4f7d06c02d04abc0005731ecb5d08
WHQ_NT_DOMAIN\MD049439 <mihai.dragan000@gmail.com>
2022-09-15 16:02:37 +0300

    Settings and patient

--- BunnyGene/app/src/main/java/com/example/bunnygene/web/Settings.java
+++ BunnyGene/app/src/main/java/com/example/bunnygene/web/Settings.java
@@ -1,6 +1,7 @@
 package com.example.bunnygene.web;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +10,26 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.bunnygene.R;
+import com.example.bunnygene.services.data.DBHelper;
+import com.example.bunnygene.services.helpers.CSVReader;
+import com.example.bunnygene.services.helpers.ImportHelper;
+
+import java.io.InputStream;
 
 public class Settings extends AppCompatActivity {
+
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+    private CSVReader csvReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getReadableDatabase();
+        csvReader = new CSVReader(this);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -24,8 +38,9 @@ public class Settings extends AppCompatActivity {
         Button yourButton = (Button) findViewById(R.id.reImport);
 
         yourButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                startActivity(new Intent(Settings.this, ImportDnaActivity.class));
+            public void onClick(View view){
+                InputStream is = getResources().openRawResource(R.raw.report);
+                ImportHelper.importDnaData(is, csvReader, db);
             }
         });
 

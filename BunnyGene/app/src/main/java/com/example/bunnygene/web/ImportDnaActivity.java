9d5ea6353fd2736a0b4355ccf612778feab817c6
WHQ_NT_DOMAIN\IP041677 <ioan.porancea@cerner.com>
2022-09-15 16:50:07 +0300

    fix merge

--- BunnyGene/app/src/main/java/com/example/bunnygene/web/ImportDnaActivity.java
+++ BunnyGene/app/src/main/java/com/example/bunnygene/web/ImportDnaActivity.java
@@ -2,12 +2,14 @@ package com.example.bunnygene.web;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.bunnygene.R;
+import com.example.bunnygene.ReportingActivity;
 import com.example.bunnygene.contract.GeneDTO;
 import com.example.bunnygene.services.data.DBHelper;
 import com.example.bunnygene.services.data.GenomeDAO;
@@ -45,13 +47,12 @@ public class ImportDnaActivity extends AppCompatActivity {
                 ImportHelper.importDnaData(is, csvReader, db);
             }
         });
-<<<<<<< HEAD
 
-        Intent i = new Intent(getApplicationContext(), ReportingActivity.class);
-        startActivity(i);
+
+        //Intent i = new Intent(getApplicationContext(), ReportingActivity.class);
+        //startActivity(i);
         //setContentView(R.layout.activity_reporting);
 
-=======
->>>>>>> 69fa133763aebd2e07532891b8f626601e2ebc98
+
     }
 }

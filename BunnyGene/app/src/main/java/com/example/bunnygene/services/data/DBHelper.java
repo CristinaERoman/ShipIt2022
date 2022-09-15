bb5dc62ed076782843e9017e84f53eb615cf0c82
MadalinaStelea <madalina.i.stelea@gmail.com>
2022-09-16 00:03:18 +0300

    Fixing Import DNA & reports view when no DNA was imported

--- BunnyGene/app/src/main/java/com/example/bunnygene/services/data/DBHelper.java
+++ BunnyGene/app/src/main/java/com/example/bunnygene/services/data/DBHelper.java
@@ -22,7 +22,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + GenomeDAO.TABLE_NAME);
-        onCreate(db);
+//        db.execSQL("DROP TABLE IF EXISTS " + GenomeDAO.TABLE_NAME);
+//        onCreate(db);
     }
 }
--- BunnyGene/app/src/main/java/com/example/bunnygene/web/MainActivity.java
+++ BunnyGene/app/src/main/java/com/example/bunnygene/web/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -16,17 +18,30 @@ import android.view.MenuItem;
 
 import com.example.bunnygene.R;
 import com.example.bunnygene.ReportingActivity;
+import com.example.bunnygene.services.data.DBHelper;
+import com.example.bunnygene.services.data.GenomeDAO;
 import com.example.bunnygene.services.helpers.AsyncInput;
 import com.example.bunnygene.services.helpers.AsyncJob;
+import com.example.bunnygene.services.helpers.CSVReader;
+import com.example.bunnygene.services.helpers.ImportHelper;
 import com.example.bunnygene.services.helpers.Notif;
+import com.example.bunnygene.services.helpers.RecommendationHelper;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
+    private Button buttonImport;
     private Button buttonGetFirstName;
     private Button buttonNotifyUser;
     private TextView textView;
 
+    private DBHelper dbHelper;
+    private SQLiteDatabase db;
+    private CSVReader csvReader;
+    private Context context;
 
     public DrawerLayout drawerLayout;
     public ActionBarDrawerToggle actionBarDrawerToggle;
@@ -57,6 +72,28 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             notifSent = true;
             sendNotifications();
         }
+
+        context = this;
+        dbHelper = new DBHelper(this);
+        db = dbHelper.getReadableDatabase();
+        csvReader = new CSVReader(this);
+
+        buttonImport = findViewById(R.id.buttonImport);
+        buttonImport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                InputStream is = getResources().openRawResource(R.raw.report);
+                db.execSQL("DROP TABLE IF EXISTS Genome");
+                db.execSQL(GenomeDAO.CREATE_GENOME_TABLE);
+                ImportHelper.importDnaData(is, csvReader, db);
+                Snackbar.make(view,"DNA data was successfully imported!", 5000).show();
+                RecommendationHelper.getRecommendationsForPersonalDisease(context, db);
+            }
+        });
+
+//        Intent i = new Intent(getApplicationContext(), ReportingActivity.class);
+//        startActivity(i);
+//        //setContentView(R.layout.activity_reporting);
     }
 
     // override the onOptionsItemSelected()
@@ -102,7 +139,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if (id == R.id.nav_reports) {
              i = new Intent(getApplicationContext(), ReportingActivity.class);
         } else if (id == R.id.nav_import) {
-             i = new Intent(getApplicationContext(), ImportDnaActivity.class);
+             i = new Intent(getApplicationContext(), MainActivity.class);
         } else if (id == R.id.nav_settings) {
              i = new Intent(getApplicationContext(), Settings.class);
         }

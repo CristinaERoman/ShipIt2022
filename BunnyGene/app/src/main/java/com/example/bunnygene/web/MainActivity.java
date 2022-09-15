df5eafa5915b48463f4b1134294ca3b027237483
CristinaRoman <cristina.e.roman@gmail.com>
2022-09-15 17:41:06 +0300

    add navigation menu

--- BunnyGene/app/src/main/java/com/example/bunnygene/web/MainActivity.java
+++ BunnyGene/app/src/main/java/com/example/bunnygene/web/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.bunnygene.web;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.MenuItem;
 
+import com.example.bunnygene.R;
 import com.example.bunnygene.services.helpers.AsyncInput;
 import com.example.bunnygene.services.helpers.AsyncJob;
 import com.example.bunnygene.services.helpers.Notif;
@@ -20,27 +25,42 @@ public class MainActivity extends AppCompatActivity {
 
     int counter =0;
 
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        // Create a database
-//        DBHelper dbHelper = new DBHelper(this);
-//        SQLiteDatabase db = dbHelper.getReadableDatabase();
-//
-//        PatientDTO patient = new PatientDTO("John", "Davis", "Paul", "M", "07/21/1983");
-//        PatientDAO.insertPatient(db, patient);
-//
-//        textView = findViewById(R.id.textView);
-//        buttonGetFirstName = findViewById(R.id.button);
-//        buttonGetFirstName.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                textView.setText(PatientDAO.getPatientFirstName(db));
-//            }
-//        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // drawer layout instance to toggle the menu icon to open
+        // drawer and back button to close drawer
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    // override the onOptionsItemSelected()
+    // function to implement
+    // the item click listener callback
+    // to open and close the navigation
+    // drawer when the icon is clicked
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void notify(View view) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
